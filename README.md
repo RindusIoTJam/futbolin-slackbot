06054073fa6d7b5b11ec4270dd6a83fa008c5973
Manfred Ackermann <manfred.ackermann@gmail.com>
2019-05-31 23:17:30 +0200

    Some documentation.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # futbolin-slackbot
+
+## Mission Statement
+
+Create a chat based tool to register for out kicker matches.
+
+## Installation
+
+Prerequisites
+
+- python
+- virtualenv 
+
+```
+git clone https://github.com/RindusIoTJam/slackbot.git
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Runtime
+
+```
+source slackbot/bin/activate
+python app.py
+```
+
+Set all properties in the config file `local_settings.json` 
+in the repository top directory with the content
+
+```
+{
+  "slackbot.token": "xoxo-398954462386-sRsuiJAN645vtkGxozNbkeVe",
+
+  "mqtt.hostname": "mqtt.rindus.es",
+  "mqtt.port":     8883,
+  "mqtt.secure":   "True",
+
+  "mqtt.username": "futbolin",
+  "mqtt.password": "TopfSekreet",
+  "mqtt.topic": "rindus/test/futbolin"
+}
+```
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import time
 import re
-import ssl
+import sys
 
 # 3rd party
 import paho.mqtt.client as paho
@@ -20,35 +20,41 @@ RTM_READ_DELAY = 1
 QUEUE_COUPLE_REGEX = '^\+.*<@(|[WU].+?)>.*<@(|[WU].+?)>.*'
 DEQUE_COUPLE_REGEX = '^\-.*<@(|[WU].+?)>.*<@(|[WU].+?)>.*'
 
-def on_message(client, userdata, message):
-  print("received message =", str(message.payload.decode("utf-8")))
 
-def on_log(client, userdata, level, buf):
-  print("log: ",buf)
+# def on_message(client, userdata, message):
+#   print("received message =", str(message.payload.decode("utf-8")))
 
-def on_connect(client, userdata, flags, rc):
-  print("connected ")
+
+# def on_log(client, userdata, level, buf):
+#   print("log: ",buf)
+
+
+# def on_connect(client, userdata, flags, rc):
+#   print("connected ")
 
 
 client = paho.Client()
 
-#client.on_message=on_message
+# client.on_message=on_message
 client.on_message = lambda *args: None
 client.on_log = lambda *args: None
 client.on_connect = lambda *args: None
 
-client.tls_set_context(context=None)
-client.tls_insecure_set(True)
-client.username_pw_set(config.get('mqtt.username'), config.get('mqtt.password'))
-client.connect("mqtt.rindus.es", 8883, 60)
+if config.get('mqtt.secure'):
+    client.tls_set_context(context=None)
+    client.tls_insecure_set(True)
 
-##start loop to process received messages
+client.username_pw_set(config.get('mqtt.username'), config.get('mqtt.password'))
+client.connect(config.get('mqtt.hostname'), int(config.get('mqtt.port')), 60)
+
+# start loop to process received messages
 client.loop_start()
+
 
 def handle_events(slack_events):
     for event in slack_events:
 
-        if event["type"] == "message" and not "subtype" in event:
+        if event["type"] == "message" and "subtype" not in event:
             print(event["user"] + ":" + event["text"])
             matches = re.search(QUEUE_COUPLE_REGEX, event["text"])
 
@@ -71,25 +77,11 @@ def handle_events(slack_events):
                                       text="You cannot play with a bot, stupid!")
             else:
                 if matches and (matches.group(1) != matches.group(2)):
-                    client.publish("rindus/futbolin", "+" + player_one['user']['profile']['real_name'] +
+                    client.publish(config.get('mqtt.topic'), "+" + player_one['user']['profile']['real_name'] +
                                    "/" + player_two['user']['profile']['real_name'])
                     slack_client.api_call("chat.postMessage", channel=event["channel"],
                                           text="Added " + player_one['user']['profile']['real_name'] + " & " +
-                                               player_two['user']['profile']['real_name'] + "to the queue.")
-
-                matches = re.search(DEQUE_COUPLE_REGEX, event["text"])
-                if matches:
-                    client.publish("rindus/futbolin", "-" + player_one['user']['profile']['real_name'] +
-                                   "/" + player_two['user']['profile']['real_name'])
-                    slack_client.api_call("chat.postMessage", channel=event["channel"],
-                                          text="Removed " + player_one['user']['profile']['real_name'] + " & " +
-                                               player_two['user']['profile']['real_name'] + "from queue.")
-
-                    player_one = slack_client.api_call("users.info", user=matches.group(1))['user']['profile']['real_name']
-                    player_two = slack_client.api_call("users.info", user=matches.group(2))['user']['profile']['real_name']
-                    client.publish("rindus/futbolin", "+" + player_one + "/" + player_two)
-                    slack_client.api_call("chat.postMessage", channel=event["channel"],
-                                                              text="Removed " + player_one + " & " + player_two + "to the queue.")
+                                               player_two['user']['profile']['real_name'] + " to the queue.")
 
 
 if __name__ == "__main__":
