2ee4c5d8864746dad9b2fa1b0252febacae8a83d
Manfred Ackermann <m.ackermann@douglas.de>
2019-05-31 16:56:29 +0200

    Fine-tuning regarding allowed add rules.

--- app.py
+++ app.py
@@ -29,10 +29,13 @@ def on_log(client, userdata, level, buf):
 def on_connect(client, userdata, flags, rc):
   print("connected ")
 
-client=paho.Client()
-client.on_message=on_message
-client.on_log=on_log
-client.on_connect=on_connect
+
+client = paho.Client()
+
+#client.on_message=on_message
+client.on_message = lambda *args: None
+client.on_log = lambda *args: None
+client.on_connect = lambda *args: None
 
 client.tls_set_context(context=None)
 client.tls_insecure_set(True)
@@ -46,23 +49,44 @@ def handle_events(slack_events):
     for event in slack_events:
 
         if event["type"] == "message" and not "subtype" in event:
-            print(event["text"])
+            print(event["user"] + ":" + event["text"])
             matches = re.search(QUEUE_COUPLE_REGEX, event["text"])
+
+            player_one = slack_client.api_call("users.info", user=matches.group(1))
+            player_two = slack_client.api_call("users.info", user=matches.group(2))
+
+            print(player_one['user']['is_bot'])
+            print(player_two['user']['is_bot'])
+
             if matches and (matches.group(1) == matches.group(2)):
                 slack_client.api_call("chat.postMessage", channel=event["channel"],
                                       text="You cannot clone yourself to build a couple!")
+
+            elif event["user"] not in (matches.group(1), matches.group(2)):
+                slack_client.api_call("chat.postMessage", channel=event["channel"],
+                                      text="You have to be part of the couple!")
+
+            elif player_one['user']['is_bot'] or player_two['user']['is_bot']:
+                slack_client.api_call("chat.postMessage", channel=event["channel"],
+                                      text="You cannot play with a bot, stupid!")
             else:
                 if matches and (matches.group(1) != matches.group(2)):
-                    player_one = slack_client.api_call("users.info", user=matches.group(1))['user']['profile']['display_name']
-                    player_two = slack_client.api_call("users.info", user=matches.group(2))['user']['profile']['display_name']
-                    client.publish("rindus/futbolin", "+" + player_one + "/" + player_two)
+                    client.publish("rindus/futbolin", "+" + player_one['user']['profile']['real_name'] +
+                                   "/" + player_two['user']['profile']['real_name'])
                     slack_client.api_call("chat.postMessage", channel=event["channel"],
-                                                              text="Added " + player_one + " & " + player_two + "to the queue.")
+                                          text="Added " + player_one['user']['profile']['real_name'] + " & " +
+                                               player_two['user']['profile']['real_name'] + "to the queue.")
 
                 matches = re.search(DEQUE_COUPLE_REGEX, event["text"])
                 if matches:
-                    player_one = slack_client.api_call("users.info", user=matches.group(1))['user']['profile']['display_name']
-                    player_two = slack_client.api_call("users.info", user=matches.group(2))['user']['profile']['display_name']
+                    client.publish("rindus/futbolin", "-" + player_one['user']['profile']['real_name'] +
+                                   "/" + player_two['user']['profile']['real_name'])
+                    slack_client.api_call("chat.postMessage", channel=event["channel"],
+                                          text="Removed " + player_one['user']['profile']['real_name'] + " & " +
+                                               player_two['user']['profile']['real_name'] + "from queue.")
+
+                    player_one = slack_client.api_call("users.info", user=matches.group(1))['user']['profile']['real_name']
+                    player_two = slack_client.api_call("users.info", user=matches.group(2))['user']['profile']['real_name']
                     client.publish("rindus/futbolin", "+" + player_one + "/" + player_two)
                     slack_client.api_call("chat.postMessage", channel=event["channel"],
                                                               text="Removed " + player_one + " & " + player_two + "to the queue.")
